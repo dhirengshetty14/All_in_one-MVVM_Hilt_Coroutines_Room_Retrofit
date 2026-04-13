e1cf51a16465220945c5b094308017368bc147db
dhirengshetty14 <dhirenedu@gmail.com>
2026-04-12 22:53:09 -0400

    Updated MainActivity.kt with ViewModel and wrote observeData()

--- app/src/main/java/com/projs/allinon_mvvm_diretrofitcoroutinesroom/view/MainActivity.kt
+++ app/src/main/java/com/projs/allinon_mvvm_diretrofitcoroutinesroom/view/MainActivity.kt
@@ -1,21 +1,36 @@
 package com.projs.allinon_mvvm_diretrofitcoroutinesroom.view
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.projs.allinon_mvvm_diretrofitcoroutinesroom.R
+import com.projs.allinon_mvvm_diretrofitcoroutinesroom.viewmodel.UserViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private val viewModel: UserViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        observeData()
+
+        viewModel.loadUsers()
+
+
+    }
+
+    private fun observeData() {
+        viewModel.users.observe(this){
+            users ->
+            Log.d("MainActivity", "Users: $users ")
         }
     }
 }
